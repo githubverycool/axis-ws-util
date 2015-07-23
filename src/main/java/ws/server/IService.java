70beefdcbb0f538945ff38c282e4df05a1d36846
githubverycool <githubemail@sina.com>
2015-07-24 00:40:18 +0800

    1 使用axis创建webservice demo

---
+++ src/main/java/ws/server/IService.java
@@ -0,0 +1,9 @@
+package ws.server;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+public interface IService {
+
+	public String getSuccess();
+}
---
+++ src/main/java/ws/server/impl/ServiceImpl.java
@@ -0,0 +1,19 @@
+/**
+ * ServiceImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package ws.server.impl;
+
+import ws.server.IService;
+
+public class ServiceImpl implements IService {
+
+	@Override
+	public String getSuccess() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
